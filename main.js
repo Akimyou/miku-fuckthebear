99349849cec2eb2b9860ecb3a9f7b23080f6ee53
MIKUScallion <MIKUScallion@hotmail.com>
2016-04-18 11:36:02 +0800

    添加画面自动缩放

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 window.onload = function(){
 //cc是一个全局变量
 cc.game.onStart = function(){
+    cc.view.adjustViewPort(true);
+    cc.view.enableRetina(true);
+    cc.view.setDesignResolutionSize(568, 320, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(true);
   //load resources
   //加载游戏资源（全局资源），回调
   //加载资源时会显示cocos的加载画面
